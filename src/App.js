065f1a44fb1da5339155b8c7ad86a7b4b025f20d
Sammy Cui <sammycui98@gmail.com>
2021-09-12 21:28:02 -0400

    add moving average
    moving average, bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import './App.css';
 import {Line} from 'react-chartjs-2';
 import Axios from 'axios';
 import exportFromJSON from 'export-from-json' ;
 import { registerables } from 'chart.js';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 
@@ -14,23 +19,30 @@ const divStyle = {
 
 
 function npointsMA(n, price){
+  let n_points = n * 24; 
+  let init_sum = 0;
 
-   let init_sum = 0;
-
-  for (var i = 0; i < n; i++){
-    init_sum += price[n];
+  for (var i = 0; i < n_points; i++){
+    init_sum += price[i];
 }
 
+  console.log(init_sum);
   const movemean = [];
-  for (var i = n-1; i < price.length; i++){
-      movemean.push(init_sum/n)
-      init_sum -= price[i - n + 1]
+
+  for (var i = 0; i < n_points-1; i++){
+    movemean.push(null);
+  }
+
+  for (var i = n_points-1; i < price.length; i++){
+      movemean.push(init_sum/n_points)
+      init_sum -= price[i - n_points + 1]
       init_sum += price[i + 1]
   }
   return movemean
 }
 
 
+//5 10 20 60 120 144 250 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -43,7 +55,13 @@ export default class App extends React.Component {
       date: [],
       price: [],
       graph: '',
-      showdownload: ''
+      showdownload: '',
+      avg5: false,
+      avg10: false,
+      avg20: false,
+      avg60: false
+
+
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -55,10 +73,12 @@ export default class App extends React.Component {
 
 
   handleChange(event) {
+    console.log(event.target.name, event.target.value);
     this.setState({[event.target.name]:event.target.value});
     }
 
   handleSubmit(event) {
+    event.preventDefault();
     console.log("there");
     Axios({
       method: 'post',
@@ -75,7 +95,7 @@ export default class App extends React.Component {
           query_data: res.data
         })
 
-
+ 
         var x = [];
         var y = [];
 
@@ -92,14 +112,51 @@ export default class App extends React.Component {
         const data = {
           labels: this.state.date,
           datasets: [{
-            label: "Silver price chart",
+            label: "Silver price",
             data: this.state.price,
             radius: 0,
             borderColor: 'rgb(75, 192, 192)',
             borderWidth: 1,
             fill: true,
             backgroundColor: 'rgba(75, 192, 192, 0.2)'
-          }]
+          },
+          {
+            label: "5 day average",
+            data: npointsMA(5,this.state.price),
+            radius: 0,
+            borderColor: 'rgba(255, 51, 51, 0.8)',
+            borderWidth: 1,
+            fill: false,
+            hidden: true
+          },
+          {
+            label: "20 day average",
+            data: npointsMA(20,this.state.price),
+            radius: 0,
+            borderColor: 'rgba(0, 128, 255, 0.8)',
+            borderWidth: 1,
+            fill: false,
+            hidden: true
+          },
+          {
+            label: "60 day average",
+            data: npointsMA(60,this.state.price),
+            radius: 0,
+            borderColor: 'rgba(0, 128, 255, 0.8)',
+            borderWidth: 1,
+            fill: false,
+            hidden: true
+          },
+          {
+            label: "144 day average",
+            data: npointsMA(144,this.state.price),
+            radius: 0,
+            borderColor: 'rgba(0, 128, 255, 0.8)',
+            borderWidth: 1,
+            fill: false,
+            hidden: true
+          }
+        ]
         };
 
         const options = {
@@ -109,12 +166,22 @@ export default class App extends React.Component {
                 maxTicksLimit: 30
               }
             }]
+          },
+          responsive: true,
+          maintainAspectRatio:false,
+          layout: {
+            padding: {
+              top: 10,
+              left: 80,
+              right: 5,
+              bottom: 15
+            }
           }
         }
 
         this.setState({
-          graph: <div class="chart-container">
-                  <Line
+          graph: <div className="linechart">
+                  <Line 
                     data = {data}
                     options= {options}
                     />
@@ -132,7 +199,7 @@ export default class App extends React.Component {
 
     handledwnload(event) {
       const data = this.state.query_data;
-      const filename = `silver_price_${this.state.start}-${this.state.end}`;
+      const filename = `silver_price_${this.state.from_dtime}-${this.state.to_dtime}`;
       const exportType = 'csv'
       exportFromJSON({data, filename, exportType});
     }
@@ -146,7 +213,8 @@ export default class App extends React.Component {
         return (<div> </div>);
       }
       */
-      return <button onClick={this.handledwnload}> Download </button>;
+      //return <button onClick={this.handledwnload}> Download </button>;
+      return <Button onClick={this.handledwnload} variant='outline-dark'>Download</Button>;
     }
 
 
@@ -155,38 +223,41 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-      <div>
-        <form onSubmit = {this.handleSubmit}>
-          <label>
-            Year:
-            <input
-              name="year"
-              type="text"
-              value={this.state.year}
-              onChange={this.handleChange} />
-          </label>
-          <label>
-            Start Date:
-            <input
-              name="from_dtime"
-              type="text"
-              value={this.state.from_dtime}
-              onChange={this.handleChange} />
-          </label>
-          <label>
-            End Date:
-            <input
-              name="to_dtime"
-              type="text"
-              value={this.state.to_dtime}
-              onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+      <div class="flexbox-container">
+        <Form onSubmit = {this.handleSubmit} className = 'settings'>
+          <Form.Group className="mb-3" >
+            <Form.Label>Year</Form.Label>
+              <Form.Control 
+                name="year" type="number" placeholder="YYYY"               
+                value={this.state.year}
+                onChange={this.handleChange}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>From Date</Form.Label>
+              <Form.Control 
+                name="from_dtime" type="text" placeholder="YYYY-MM-DD"               
+                value={this.state.from_dtime}
+                onChange={this.handleChange.bind(this)}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>To Date</Form.Label>
+              <Form.Control 
+                name="to_dtime"type="text" placeholder="YYYY-MM-DD"               
+                value={this.state.to_dtime}
+                onChange={this.handleChange.bind(this)}/>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        {this.state.graph}
+        <div>
+        {this.state.showdownload}
+        </div>
 
-      {this.state.graph}
-      {this.state.showdownload}
+      </div>
+      
+      
       </>
     );
   }
